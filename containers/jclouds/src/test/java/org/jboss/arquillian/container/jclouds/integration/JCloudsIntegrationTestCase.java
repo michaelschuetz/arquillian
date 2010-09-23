d4bf1a6bceb3e25124a279ebc208705e646e525d
Aslak Knutsen <aslak@redhat.com>
2010-09-23 20:01:42 +0200

    ARQ-285 Add simple Integration test case to see that deploy/undeploy etc works

---
+++ containers/jclouds/src/test/java/org/jboss/arquillian/container/jclouds/integration/JCloudsIntegrationTestCase.java
@@ -0,0 +1,58 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.arquillian.container.jclouds.integration;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * JCloudsIntegrationTestCase
+ *
+ * @author <a href="mailto:aslak@redhat.com">Aslak Knutsen</a>
+ * @version $Revision: $
+ */
+@RunWith(Arquillian.class)
+public class JCloudsIntegrationTestCase
+{
+
+   @Deployment
+   public static JavaArchive createDeployment() 
+   {
+      return ShrinkWrap.create(JavaArchive.class)
+               .addClass(ServiceManager.class)
+               .addManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+   }
+   
+   @Inject 
+   private ServiceManager manager;
+   
+   @Test
+   public void shouldBeAbleToExecuteAndInject() throws Exception
+   {
+      Assert.assertNotNull(manager);
+      
+      Assert.assertEquals("Hello", manager.sayHello());
+   }
+}
