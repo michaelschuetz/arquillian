bee07ddfec4ac2d6150a2df963b8b09685d43948
Adrian Cole <adrian@jclouds.org>
2010-09-24 16:06:07 -0700

    updated to show an example of how to start jboss

--- containers/jclouds/src/main/java/org/jboss/arquillian/container/jclouds/JCloudsContainer.java
+++ containers/jclouds/src/main/java/org/jboss/arquillian/container/jclouds/JCloudsContainer.java
@@ -16,10 +16,10 @@
  */
 package org.jboss.arquillian.container.jclouds;
 
-import static org.jclouds.compute.domain.OsFamily.UBUNTU;
-import static org.jclouds.compute.options.TemplateOptions.Builder.inboundPorts;
+import static org.jclouds.compute.options.TemplateOptions.Builder.blockOnComplete;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.Set;
 
@@ -37,147 +37,126 @@ import org.jclouds.compute.ComputeServiceContextFactory;
 import org.jclouds.compute.domain.NodeMetadata;
 import org.jclouds.compute.domain.Template;
 import org.jclouds.compute.predicates.NodePredicates;
-import org.jclouds.io.Payloads;
 import org.jclouds.logging.log4j.config.Log4JLoggingModule;
 import org.jclouds.ssh.jsch.config.JschSshClientModule;
 
+import com.google.common.base.Charsets;
+import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.io.Files;
 
 /**
  * JCloudsContainer
- *
+ * 
  * @author <a href="mailto:aslak@redhat.com">Aslak Knutsen</a>
  * @version $Revision: $
  */
-public class JCloudsContainer implements DeployableContainer
-{
-   /* (non-Javadoc)
-    * @see org.jboss.arquillian.spi.DeployableContainer#setup(org.jboss.arquillian.spi.Context, org.jboss.arquillian.spi.Configuration)
+public class JCloudsContainer implements DeployableContainer {
+
+   /*
+    * (non-Javadoc)
+    * 
+    * @see org.jboss.arquillian.spi.DeployableContainer#setup(org.jboss.arquillian.spi.Context,
+    * org.jboss.arquillian.spi.Configuration)
     */
-   public void setup(Context context, Configuration configuration)
-   {
+   public void setup(Context context, Configuration configuration) {
       JCloudsConfiguration config = configuration.getContainerConfig(JCloudsConfiguration.class);
 
-      ComputeServiceContext computeContext = new ComputeServiceContextFactory().createContext(
-            config.getProvider(), 
-            config.getIdentity(), 
-            config.getCredential(),
-            ImmutableSet.of(
-                  new Log4JLoggingModule(), 
-                  new JschSshClientModule()));
+      ComputeServiceContext computeContext = new ComputeServiceContextFactory().createContext(config.getProvider(),
+               config.getIdentity(), config.getCredential(), ImmutableSet.of(new Log4JLoggingModule(),
+                        new JschSshClientModule()));
 
-      // Bind the ComputeServiceContext to the Arquillian Context so other Handlers can interact with it
+      // Bind the ComputeServiceContext to the Arquillian Context so other Handlers can interact
+      // with it
       context.add(ComputeServiceContext.class, computeContext);
-      
+
       ComputeService computeService = computeContext.getComputeService();
 
       // TOOD: should be extracted out into some sort of configuration..
-      Template template = computeService.templateBuilder()
-                                 //.smallest()
-                                 .osFamily(UBUNTU)
-                                 //.minRam(1000)
-                                 .options(
-                                       inboundPorts(1099, config.getRemoteServerHttpPort())
-                                       .authorizePublicKey(
-                                             Payloads.newFilePayload(
-                                                   new File(System.getProperty("user.home") + "/.ssh/id_rsa.pub")))
-                                       .runScript(Payloads.newStringPayload(createScriptBoot())))
-                                 .build();
-      
+      Template template = computeService.templateBuilder().options(blockOnComplete(false).blockOnPort(1099, 300).inboundPorts(22, 1099, 8080))
+               .build();
+  
+      try {
+         // note this is a dependency on the template resolution
+         template.getOptions().runScript(
+                  RunScriptData.createScriptInstallAndStartJBoss(Files.toString(new File(System.getProperty("user.home")
+                           + "/.ssh/id_rsa.pub"), Charsets.UTF_8), template.getImage()
+                           .getOperatingSystem()));
+
+      } catch (IOException e) {
+         Throwables.propagate(e);
+      }
+
       context.add(Template.class, template);
    }
 
-   /* (non-Javadoc)
+   /*
+    * (non-Javadoc)
+    * 
     * @see org.jboss.arquillian.spi.DeployableContainer#start(org.jboss.arquillian.spi.Context)
     */
-   public void start(Context context) throws LifecycleException
-   {
+   public void start(Context context) throws LifecycleException {
       JCloudsConfiguration config = context.get(Configuration.class).getContainerConfig(JCloudsConfiguration.class);
       ComputeServiceContext computeContext = context.get(ComputeServiceContext.class);
       Template template = context.get(Template.class);
-      
-      try
-      {
+
+      try {
          // start the nodes
-         Set<? extends NodeMetadata> startedNodes = computeContext.getComputeService().runNodesWithTag(
-               config.getTag(), 
-               config.getNodeCount(), 
-               template);
-         
+         Set<? extends NodeMetadata> startedNodes = computeContext.getComputeService().runNodesWithTag(config.getTag(),
+                  config.getNodeCount(), template);
+
          context.add(NodeOverview.class, new NodeOverview(startedNodes));
-      }
-      catch (Exception e) 
-      {
+      } catch (Exception e) {
          throw new LifecycleException("Could not start nodes", e);
       }
    }
 
-   /* (non-Javadoc)
-    * @see org.jboss.arquillian.spi.DeployableContainer#deploy(org.jboss.arquillian.spi.Context, org.jboss.shrinkwrap.api.Archive)
+   /*
+    * (non-Javadoc)
+    * 
+    * @see org.jboss.arquillian.spi.DeployableContainer#deploy(org.jboss.arquillian.spi.Context,
+    * org.jboss.shrinkwrap.api.Archive)
     */
-   public ContainerMethodExecutor deploy(Context context, Archive<?> archive) throws DeploymentException
-   {
+   public ContainerMethodExecutor deploy(Context context, Archive<?> archive) throws DeploymentException {
       JCloudsConfiguration config = context.get(Configuration.class).getContainerConfig(JCloudsConfiguration.class);
       NodeOverview nodeOverview = context.get(NodeOverview.class);
       String publicAddress = nodeOverview.getStartedNodes().iterator().next().getPublicAddresses().iterator().next();
 
-      // TODO: push Archive to BlobStore 
-      
-      try 
-      {
-         return new ServletMethodExecutor(
-               new URL(
-                     "http",
-                     publicAddress,
-                     config.getRemoteServerHttpPort(),
-                     "/")
-               );
-      } 
-      catch (Exception e) 
-      {
+      // TODO: push Archive to BlobStore
+
+      try {
+         return new ServletMethodExecutor(new URL("http", publicAddress, config.getRemoteServerHttpPort(), "/"));
+      } catch (Exception e) {
          throw new RuntimeException("Could not create ContianerMethodExecutor", e);
       }
    }
-   
-   /* (non-Javadoc)
-    * @see org.jboss.arquillian.spi.DeployableContainer#undeploy(org.jboss.arquillian.spi.Context, org.jboss.shrinkwrap.api.Archive)
+
+   /*
+    * (non-Javadoc)
+    * 
+    * @see org.jboss.arquillian.spi.DeployableContainer#undeploy(org.jboss.arquillian.spi.Context,
+    * org.jboss.shrinkwrap.api.Archive)
     */
-   public void undeploy(Context context, Archive<?> archive) throws DeploymentException
-   {
+   public void undeploy(Context context, Archive<?> archive) throws DeploymentException {
       /*
-      NodeOverview nodeOverview = context.get(NodeOverview.class);
-      String publicAddress = nodeOverview.getStartedNodes().iterator().next().getPublicAddresses().iterator().next();
-      */
-      // TODO: remove Archive from BlobStore 
-      
+       * NodeOverview nodeOverview = context.get(NodeOverview.class); String publicAddress =
+       * nodeOverview.getStartedNodes().iterator().next().getPublicAddresses().iterator().next();
+       */
+      // TODO: remove Archive from BlobStore
+
    }
 
-   /* (non-Javadoc)
+   /*
+    * (non-Javadoc)
+    * 
     * @see org.jboss.arquillian.spi.DeployableContainer#stop(org.jboss.arquillian.spi.Context)
     */
-   public void stop(Context context) throws LifecycleException
-   {
+   public void stop(Context context) throws LifecycleException {
       JCloudsConfiguration config = context.get(Configuration.class).getContainerConfig(JCloudsConfiguration.class);
       ComputeServiceContext computeContext = context.get(ComputeServiceContext.class);
 
-      computeContext.getComputeService().destroyNodesMatching(
-            NodePredicates.withTag(config.getTag()));
+      computeContext.getComputeService().destroyNodesMatching(NodePredicates.withTag(config.getTag()));
 
    }
 
-   // TODO: extract out to a SPI for each container etc, on how to install it self
-   protected String createScriptBoot()
-   {
-      return createScriptInstallJava() + "\n" + createScriptInstallAndStartJBoss();
-   }
-   
-   protected String createScriptInstallJava()
-   {
-      return "";
-   }
-
-   protected String createScriptInstallAndStartJBoss()
-   {
-      return "";
-   }
 }
